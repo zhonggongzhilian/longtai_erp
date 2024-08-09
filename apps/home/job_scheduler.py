0a4b9188aa151b8bf35dbc7baf44b0a2f8cfba5b
huiofficial <tzattack@gmail.com>
2024-08-09 14:15:45 +0800

    update job scheduler with working time and changeover

--- apps/home/job_scheduler.py
+++ apps/home/job_scheduler.py
@@ -1,9 +1,8 @@
-import time
 from datetime import datetime, timedelta
 
 from django.utils import timezone
 
-from .models import Device, Order, Process, Task
+from .models import Device, Order, Process, Task, Product
 
 
 def is_max_process(order_product):
@@ -52,7 +51,6 @@ def remove_order_products_with_outside_process(order_products):
                 Device.objects.create(
                     device_name=device_name
                 )
-                print(f"adding device: {device_name}")
 
             # 如果工序的 is_outside 值为 1，标记订单产品进行移除
             if process.is_outside == 1:
@@ -62,6 +60,44 @@ def remove_order_products_with_outside_process(order_products):
     # 从 order_products 列表中移除标记的订单产品
     for order_product in to_remove:
         order_products.remove(order_product)
+
+
+def add_working_time(start_time, duration=0):
+    """增加工作时间，跳过非工作时间"""
+
+    end_time = start_time
+
+    while duration >= 0:
+        work_start_morning = end_time.replace(hour=9, minute=0, second=0, microsecond=0)
+        work_end_morning = end_time.replace(hour=12, minute=0, second=0, microsecond=0)
+        work_start_afternoon = end_time.replace(hour=13, minute=0, second=0, microsecond=0)
+        work_end_afternoon = end_time.replace(hour=18, minute=0, second=0, microsecond=0)
+
+        if end_time < work_start_morning:
+            end_time = work_start_morning
+        elif end_time < work_end_morning:
+            available_time = (work_end_morning - end_time).seconds / 60
+            if duration <= available_time:
+                end_time += timedelta(minutes=duration)
+                duration = -1
+            else:
+                duration -= available_time
+                end_time = work_start_afternoon
+        elif end_time < work_start_afternoon:
+            end_time = work_start_afternoon
+        elif end_time < work_end_afternoon:
+            available_time = (work_end_afternoon - end_time).seconds / 60
+            if duration <= available_time:
+                end_time += timedelta(minutes=duration)
+                duration = -1
+            else:
+                duration -= available_time
+                end_time = work_start_morning + timedelta(days=1)
+        else:
+            # 处理结束时间在下午工作结束后的情况
+            end_time = work_start_morning + timedelta(days=1)
+
+    return end_time
 
 
 def schedule_production(start_date_str='2024-01-01'):
@@ -75,9 +111,7 @@ def schedule_production(start_date_str='2024-01-01'):
         key=lambda p: p.order.order_end_date
     )
 
-    # remove_false_order_products(order_products)
-
-    # 使用一个字典缓存所有工序，避免重复查询
+    # 字典缓存所有工序
     process_cache = {}
     for order in orders:
         for order_product in order.products.filter(is_done=False):
@@ -92,7 +126,7 @@ def schedule_production(start_date_str='2024-01-01'):
                 }
 
     start_date = timezone.make_aware(datetime.strptime(start_date_str, '%Y-%m-%d'))
-    current_time = start_date
+    current_time = add_working_time(start_date)
 
     devices = Device.objects.all()
 
@@ -101,13 +135,19 @@ def schedule_production(start_date_str='2024-01-01'):
             # 如果当前时间在设备的使用时间范围内，则跳过
             if device.start_time <= current_time < device.end_time:
                 continue
-            # print(f"{current_time.strftime('%Y-%m-%d %H:%M:%S')} {device.device_name} is free.")
-            #
+
             for order_product in order_products:
-                print(f"{current_time=}, {order_product.end_time=}")
-                # time.sleep(1)
+
+                # debug
+                if order_product.product_code != "MMA-511-11001" or order_product.order.order_code != "587011":
+                    order_products.remove(order_product)
+                    continue
+                # print(
+                #     f"{order_product.order.order_code}\t{order_product.product_code}\t{device.device_name}\t{current_time}\t{order_product.end_time}")
+
                 if current_time < order_product.end_time:
                     continue
+
                 processes = process_cache.get(order_product.id, {})
                 next_process_i = min((pi for pi in processes.keys() if pi > order_product.cur_process_i), default=None)
                 process = processes.get(next_process_i)
@@ -115,8 +155,18 @@ def schedule_production(start_date_str='2024-01-01'):
                 if process:
                     # 进行排产
                     duration = process['process_duration']
-                    device.start_time = current_time
-                    device.end_time = current_time + timedelta(minutes=duration)
+
+                    # 更新设备换型信息
+                    raw_code = Product.objects.get(product_code=order_product.product_code).raw_code
+                    is_changeover = 0
+                    if not device.raw or device.raw != raw_code:
+                        print(f"{device.raw}, {raw_code}")
+                        duration += float(device.changeover_time)
+                        is_changeover = 1
+                    device.raw = raw_code
+
+                    device.start_time = add_working_time(current_time)
+                    device.end_time = add_working_time(current_time, duration)
 
                     # 更新产品的当前工序索引
                     order_product.cur_process_i = process['process_i']
@@ -135,7 +185,8 @@ def schedule_production(start_date_str='2024-01-01'):
                         process_i=process['process_i'],
                         process_name=process['process_name'],
                         device_name=device.device_name,
-                        product_num=product_num
+                        product_num=product_num,
+                        is_changeover=is_changeover
                     )
 
                     # 移除已处理的订单产品，如果当前工序是最大序号工序
